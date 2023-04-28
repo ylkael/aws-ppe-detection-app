dddeb7fcaea0d48c0b6ed885d1c8308b75d5cc26
Ylkael <ylkael@hotmail.com>
2023-04-28 10:49:02 +0300

    s3 bucket for image input

--- lib/ppe_app-stack.ts
+++ lib/ppe_app-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PpeAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PpeAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  
+    // S3 bucket for storing image input
+    const inputbucket = new s3.Bucket(this, 'ImageInputBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL
+    });
   }
 }
