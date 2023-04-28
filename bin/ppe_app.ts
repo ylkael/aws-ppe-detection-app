49b79a4ca7285e7bcdf14c85de4432a26ecde69d
Ylkael <ylkael@hotmail.com>
2023-04-28 14:18:00 +0300

    pharmacy + workshop stacks

--- bin/ppe_app.ts
+++ bin/ppe_app.ts
@@ -4,18 +4,15 @@ import * as cdk from 'aws-cdk-lib';
 import { PpeAppStack } from '../lib/ppe_app-stack';
 
 const app = new cdk.App();
-new PpeAppStack(app, 'PpeAppStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const pharmacyClient = new PpeAppStack(app, 'PharmacyPpeStack', {
+  DETECTHEADCOVER: false,
+  DETECTFACECOVER: true,
+  DETECTHANDCOVER: true
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const workshopClient = new PpeAppStack(app, 'WorkshopPpeStack', {
+  DETECTHEADCOVER: true,
+  DETECTFACECOVER: true,
+  DETECTHANDCOVER: true
 });
