ee0f944d6c7044f669c23c5dbe7417d2e95ad7d7
Ylkael <ylkael@hotmail.com>
2023-04-28 10:37:15 +0300

    Initial commit

---
+++ test/ppe_app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PpeApp from '../lib/ppe_app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ppe_app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PpeApp.PpeAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
