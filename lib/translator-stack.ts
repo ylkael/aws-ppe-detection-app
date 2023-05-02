e6f3c3a7f162356df5205b4938bbf165fb49cf75
Ylkael <ylkael@hotmail.com>
2023-05-02 12:15:01 +0300

    stackprops

--- lib/translator-stack.ts
+++ lib/translator-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import * as sns from 'aws-cdk-lib/aws-sns';
 import { Construct } from 'constructs';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
@@ -6,9 +7,12 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as path from 'path';
 import { SnsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 
-
+interface TranslatorStackProps extends cdk.StackProps {
+    snsTopic: Array<sns.Topic>;
+    translateTo: string;
+}
 export class TranslatorStack extends cdk.Stack{
-    constructor (scope: Construct, id: string, props?: cdk.StackProps){
+    constructor (scope: Construct, id: string, props?: TranslatorStackProps){
         super(scope, id, props);
 
     }
